d54a03d9410145fb1c060f2f186930a48b1f739a
Wendy Liga <wendy.devs@gmail.com>
2018-11-21 08:49:11 +0700

    add shapeArea

---
+++ Code Signal/shapeArea/shapeArea.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "https://app.codesignal.com/arcade/intro/level-2/yuGuHvcCaFCKk56rJ"
+
+func shapeArea(n: Int) -> Int {
+    return(n*n + ((n-1) * (n-1)))
+}
+
+shapeArea(n: 5)
