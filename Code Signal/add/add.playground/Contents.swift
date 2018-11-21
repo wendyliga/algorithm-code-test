02e7b4cb20eb3e5279aa812b73846a3043472fd9
Wendy Liga <wendy.devs@gmail.com>
2018-11-21 08:57:36 +0700

    add add

---
+++ Code Signal/add/add.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "https://app.codesignal.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa"
+
+func add(param1: Int, param2: Int) -> Int {
+    return param1 + param2
+}
+
+add(param1: 1, param2: 1)
